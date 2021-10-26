961e6b3de620f1a6af71e8d1daf385783529b425
dsclementsen <davur@davur.net>
2021-10-26 19:22:40 +1100

    Define models

--- filearchive/models.py
+++ filearchive/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+class Root(models.Model):
+    name = models.CharField(max_length=255)
+    abspath = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Path(models.Model):
+    class FileType(models.TextChoices):
+        DIRECTORY = 'd', 'Directory'
+        FILE = 'f', 'File'
+
+    name = models.CharField(max_length=255, blank=False, null=False)
+    root = models.ForeignKey('Root', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self',
+        related_name='children',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        )
+    file_type = models.CharField(
+        max_length=1,
+        choices=FileType.choices,
+    )
+    modified_time = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
